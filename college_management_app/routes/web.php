34400814fe1ec26f0c9e9efae80baa8331cf9c39
Miguel Abela <abelamiguel@gmail.com>
2025-03-12 22:18:56 +0000

    Modified the basic named routes and added basic views for the colleges and students

--- college_management_app/routes/web.php
+++ college_management_app/routes/web.php
@@ -17,39 +17,38 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Note: The '.' indicates that the blade.php view files are within a sub-folder
+
 // ------------------------------- College Named Routes -------------------------------
 // This route will show the user a list of all the colleges
-Route::get('/college', function() {
-    return "<h1>All Colleges</h1>";
-})->name('college.index');
+Route::get('/colleges', function() {
+    return view('colleges.index');
+})->name('colleges.index');
 
 // This route will direct the user to create a new college
-Route::get('/college/create', function() {
-    return "<h1>Add a new College</h1>";
-})->name('college.create');
+Route::get('/colleges/create', function() {
+    return view('colleges.create');
+})->name('colleges.create');
 
-// This route that will enable the user to update specific college details
-Route::get('/college/{id}/edit', function($id) {
-    return App\Models\College::find($id);
-})->name('college.edit');
+// This route that will enable the user to view a specific college
+Route::get('/colleges/{id}', function($id) {
+    $college = App\Models\College::find($id);
+    return view('colleges.show', compact('college'));
+})->name('colleges.show');
 
 // ------------------------------- Student Named Routes -------------------------------
 // This route will show the user a list of all the students
 Route::get('/students', function() {
-    return "<h1>All Students</h1>";
+    return view('students.index'); 
 })->name('students.index');
 
 // This route will direct the user to create a new student
 Route::get('/students/create', function() {
-    return "<h1>Add a new Student</h1>";
+    return view('students.create');
 })->name('students.create');
 
-// This route will enable the user to udpate the deatils of a specific student
-Route::get('/students/{id}/edit', function($id) {
-    return App\Models\Student::find($id);
-})->name('students.edit');
-
-// This route will enable the user to delete a specific student
+// This route will enable the user view a specific student
 Route::get('/students/{id}', function($id) {
-    return App\Models\Student::find($id);
-})->name('students.destroy');
+    $student = App\Models\Student::find($id);
+    return view('students.show', compact('student'));
+})->name('students.show');
