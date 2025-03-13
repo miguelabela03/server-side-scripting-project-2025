ab6798128b3f13d8650577236c9de0ee249cc377
Miguel Abela <abelamiguel@gmail.com>
2025-03-13 21:04:17 +0000

    Added a partial view for the college filter

---
+++ college_management_app/resources/views/students/_filter.blade.php
@@ -0,0 +1,13 @@
+<div class="card-body">
+    <div class="row align-items-center">
+        <div class="col-md-6">
+            <div class="input-group mb-3">
+                <select class="custom-select">
+                    <option value="" selected>All Colleges</option>
+                    <option value="1">College 1</option>
+                    <option value="2">College 2</option>
+                </select>
+            </div>
+        </div>
+    </div>
+</div>
--- college_management_app/resources/views/students/index.blade.php
+++ college_management_app/resources/views/students/index.blade.php
@@ -14,19 +14,7 @@
                                 </a>                                                                
                             </div>
                         </div>
-                        <div class="card-body">
-                            <div class="row align-items-center">
-                                <div class="col-md-6">
-                                    <div class="input-group mb-3">
-                                        <select class="custom-select">
-                                            <option value="" selected>All Colleges</option>
-                                            <option value="1">College 1</option>
-                                            <option value="2">College 2</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        @include('students._filter')
                         <table class="table table-striped table-hover">
                             <thead>
                                 <tr>
