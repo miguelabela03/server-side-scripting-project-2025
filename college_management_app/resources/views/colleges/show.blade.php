ac0544f28034f3d6c12f419cf66b488b9f335b51
Miguel Abela <abelamiguel@gmail.com>
2025-03-13 13:31:10 +0000

    Added a basic colleges show layout template

---
+++ college_management_app/resources/views/colleges/show.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.main')
+
+@section('content')
+    <main class="py-5">
+        <div class="container">
+            <div class="row justify-content-md-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header card-title position-relative">
+                            <strong>College Information</strong>
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group row">
+                                        <label for="name" class="col-md-3 col-form-label">College Name</label>
+                                        <div class="col-md-9">
+                                            <p class="form-control-plaintext text-muted">College 1</p>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <label for="address" class="col-md-3 col-form-label">Address</label>
+                                        <div class="col-md-9">
+                                            <p class="form-control-plaintext text-muted">Address 1</p>
+                                        </div>
+                                    </div>
+                                    <hr>
+                                    <div class="form-group row mb-0">
+                                        <div class="col-md-6">
+                                            <a href="#" class="btn btn-outline-primary">Edit</a>
+                                        </div>
+                                        <div class="col-md-6 text-right">
+                                            <a href="{{ route('colleges.index') }}" class="btn btn-outline-secondary">Cancel</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+@endsection
