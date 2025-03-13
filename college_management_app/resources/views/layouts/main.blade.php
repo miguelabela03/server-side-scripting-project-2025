9177242a38f636950887294c87014ea6ec2b8a59
Miguel Abela <abelamiguel@gmail.com>
2025-03-13 13:34:51 +0000

    Added routes to the main layout

--- college_management_app/resources/views/layouts/main.blade.php
+++ college_management_app/resources/views/layouts/main.blade.php
@@ -19,7 +19,7 @@
     <!-- navbar -->
     <nav class="navbar navbar-expand-lg navbar-light">
         <div class="container">
-            <a class="navbar-brand text-uppercase" href="student.html">
+            <a class="navbar-brand text-uppercase" href={{ route('students.index') }}>
                 <strong>Management System</strong>
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-toggler"
@@ -31,7 +31,7 @@
             <div class="collapse navbar-collapse" id="navbar-toggler">
                 <ul class="navbar-nav">
                     <li class="nav-item active">
-                        <a href="student.html" class="nav-link">
+                        <a href={{ route('students.index') }} class="nav-link">
                             <div class="nav-icon-text">
                                 <ion-icon name="person-outline"></ion-icon>
                                 <span>Students</span>
@@ -39,7 +39,7 @@
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a href="college.html" class="nav-link">
+                        <a href={{ route('colleges.index') }} class="nav-link">
                             <div class="nav-icon-text">
                                 <ion-icon name="business-outline"></ion-icon>
                                 <span>Colleges</span>
