6874a12f623703b96211939a2ef527cb18582fda
Miguel Abela <abelamiguel@gmail.com>
2025-03-13 13:14:52 +0000

    Added a a basic show students layout template

--- college_management_app/resources/views/students/show.blade.php
+++ college_management_app/resources/views/students/show.blade.php
@@ -1,1 +1,70 @@
-<h1>Student Details</h1>
+@extends('layouts.main')
+
+@section('content')
+    <main class="py-5">
+        <div class="container">
+            <div class="row justify-content-md-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header card-title position-relative">
+                            <strong>Student Details</strong>
+                            <div class="position-absolute top-right">
+                                <a href="#" class="btn btn-danger">Delete</a>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group row">
+                                        <label for="name" class="col-md-3 col-form-label">Student Name</label>
+                                        <div class="col-md-9">
+                                            <p class="form-control-plaintext text-muted">Tom</p>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <label for="email" class="col-md-3 col-form-label">Email</label>
+                                        <div class="col-md-9">
+                                            <p class="form-control-plaintext text-muted">tom@gmail.com</p>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <label for="phone" class="col-md-3 col-form-label">Phone</label>
+                                        <div class="col-md-9">
+                                            <p class="form-control-plaintext text-muted">12345678</p>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <label for="dob" class="col-md-3 col-form-label">Date of Birth</label>
+                                        <div class="col-md-9">
+                                            <p class="form-control-plaintext text-muted">01/01/20001</p>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <label for="college_id" class="col-md-3 col-form-label">College Name</label>
+                                        <div class="col-md-9">
+                                            <p class="form-control-plaintext text-muted">College 1</p>
+                                        </div>
+                                    </div>
+
+                                    <hr>
+                                    <div class="form-group row mb-0">
+                                        <div class="col-md-6">
+                                            <a href="#" class="btn btn-outline-primary">Edit</a>
+                                        </div>
+                                        <div class="col-md-6 text-right">
+                                            <a href="{{ route('students.index') }}" class="btn btn-outline-secondary">Cancel</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+@endsection
