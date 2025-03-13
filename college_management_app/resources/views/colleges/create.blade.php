eae811511ed42642a9c9253d8105947cb21e644b
Miguel Abela <abelamiguel@gmail.com>
2025-03-13 13:27:02 +0000

    Added a basic colleges create layout template

--- college_management_app/resources/views/colleges/create.blade.php
+++ college_management_app/resources/views/colleges/create.blade.php
@@ -1,1 +1,49 @@
-<h1>Add a New College</h1>
+@extends('layouts.main')
+
+@section('content')
+    <main class="py-5">
+        <div class="container">
+            <div class="row justify-content-md-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header card-title position-relative">
+                            <strong>Add New College</strong>
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group row">
+                                        <label for="name" class="col-md-3 col-form-label">College Name</label>
+                                        <div class="col-md-9">
+                                            <input type="text" name="name" id="name" class="form-control is-invalid">
+                                            <div class="invalid-feedback">
+                                                Please choose a college name.
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <label for="name" class="col-md-3 col-form-label">Address</label>
+                                        <div class="col-md-9">
+                                            <textarea name="address" id="address" rows="3"
+                                                class="form-control"></textarea>
+                                        </div>
+                                    </div>
+                                    <hr>
+                                    <div class="form-group row mb-0">
+                                        <div class="col-md-6">
+                                            <button type="submit" class="btn btn-success">Save</button>
+                                        </div>
+                                        <div class="col-md-6 text-right">
+                                            <a href="{{ route('colleges.index') }}" class="btn btn-outline-secondary">Cancel</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+@endsection
