0533b524676db18aeb32eb5ba254ada2f0abc64e
Miguel Abela <abelamiguel@gmail.com>
2025-03-14 00:05:47 +0000

    Implemented and tested student form validation

--- college_management_app/app/Http/Controllers/StudentController.php
+++ college_management_app/app/Http/Controllers/StudentController.php
@@ -29,10 +29,29 @@ class StudentController extends Controller
         return view('students.create', compact('colleges'));
     }
 
-    // This function will store the student form data
+    // This function will validate and store the student form data
+    // Validation -> All fields are required,
+    // Name: only letters and spaces are accepted (in case the name consits of two words or more)
+    // Email: the laravel email rule is used but using regex I am checking that;
+    //     a) at least one character is before the @ symbol ([^@]+@)
+    //     b) at least one letter is after the @ symbol and before a dot ([^@]+\.)
+    //     c) at least 2 letters are present after the dot ([a-zA-Z]{2,}$)
+    // Phone: the number 8 ensures that the phone number has exactly 8 digits (it acts as a min and max),
+    //        and the 'digits' rule only accepts numbers without a - or +
+    // Date of Birth: date is used to check for a valid date,
+    //                and 'before:today' is used as  date of birth can only be in the past and not in the future
+    // College Id: 'exists' checks that the college_id is present within the colleges tables
     public function store(Request $request) {
+        $request->validate([
+            'name' => 'required|regex:/^[a-zA-Z ]+$/', 
+            'email' => 'required|email|regex:/^[^@]+@[^@]+\.[a-zA-Z]{2,}$/',
+            'phone' => 'required|digits:8',
+            'dob' => 'required|date|before:today',
+            'college_id' => 'required|exists:colleges,id',
+        ]);
+    
         dd($request->all());
-    }
+    }    
 
     // This function will display specific student details
     public function show($id) {
--- college_management_app/resources/views/students/_form.blade.php
+++ college_management_app/resources/views/students/_form.blade.php
@@ -3,42 +3,69 @@
         <div class="form-group row">
             <label for="name" class="col-md-3 col-form-label">Student Name</label>
             <div class="col-md-9">
-                <input type="text" name="name" id="name" class="form-control is-invalid">
-                <div class="invalid-feedback">
-                    Please choose a student name.
-                </div>
+                <input type="text" name="name" id="name" class="form-control @error('name') is-invalid @enderror">
+
+                @error('name')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
         </div>
 
         <div class="form-group row">
             <label for="email" class="col-md-3 col-form-label">Email</label>
             <div class="col-md-9">
-                <input type="text" name="email" id="email" class="form-control">
+                <input type="text" name="email" id="email" class="form-control @error('email') is-invalid @enderror">
+
+                @error('email')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
         </div>
 
         <div class="form-group row">
             <label for="phone" class="col-md-3 col-form-label">Phone</label>
             <div class="col-md-9">
-                <input type="text" name="phone" id="phone" class="form-control">
+                <input type="text" name="phone" id="phone" class="form-control @error('phone') is-invalid @enderror">
+
+                @error('phone')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
         </div>
 
         <div class="form-group row">
             <label for="dob" class="col-md-3 col-form-label">Date of Birth</label>
             <div class="col-md-9">
-                <input type="date" name="dob" id="dob" class="form-control">
+                <input type="date" name="dob" id="dob" class="form-control @error('dob') is-invalid @enderror">
+
+                @error('dob')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
         </div>
 
         <div class="form-group row">
             <label for="college_id" class="col-md-3 col-form-label">College Name</label>
             <div class="col-md-9">
-                <select name="college_id" id="college_id" class="form-control">
+                <select name="college_id" id="college_id" class="form-control @error('college_id') is-invalid @enderror">
                     @foreach ($colleges as $id => $name)
                         <option value="{{ $id }}">{{ $name }}</option>
                     @endforeach
                 </select>
+
+                @error('college_id')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
         </div>
 
